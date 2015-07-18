2439cc05c00f1f21d99b9cc2aba80dfb5e1cc842
emrekose26 <e.kose26@gmail.com>
2015-07-18 15:18:07 +0300

    Profil sayfası iskeleti oluşturuldu

---
+++ profil.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Profil</title>
+	<link rel="icon" href="assets/images/palms-icon.ico"/>
+
+	<!--Styles-->
+	<link rel="stylesheet" href="assets/css/profil.css"/>
+</head>
+<body>
+	<ul id="profil-menu">
+		<li><a href="profil.php">Profilim</a></li>
+		<li><a href="arkadas.php">Arkadaşlarım</a></li>
+		<li><a href="mesaj.php">Mesajlarım</a></li>
+		<li><a href="fotograf.php">Fotoğraflarım</a></li>
+		<li><a href="video.php">Videolarım</a></li>
+		<li><a href="muzik.php">Müziklerim</a></li>
+	</ul>
+</body>
+</html>
