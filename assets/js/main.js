7fa208d8ac6ef786a600c9a065bafabf70bb4729
emrekose26 <e.kose26@gmail.com>
2015-07-24 18:25:51 +0300

    main.js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-
+	//profil bilgileri detaylı bilgiler göster-gizle kısmı
+	var sayac = 0;
+	$('#profil-detay-bilgiler').hide();
+	$(".profil-bilgiler-link a").click(function(){
+		$("#profil-detay-bilgiler").slideToggle(100);
+	if(sayac == 0){
+		$(this).text("Detaylı Bilgileri Gizle");
+		sayac++;
+	}else{
+		$(this).text("Detaylı Bilgileri Göster");
+		sayac = 0;
+	}
+	});
 });
